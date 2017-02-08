c95e0d78b586d104e40d2cbea1f2baf0cdb9d6f0
Krunal <krunalm@emgage.com>
2017-02-08 17:01:10 +0530

    Some change 2!

--- src/pages/login/page.js
+++ src/pages/login/page.js
@@ -11,7 +11,7 @@ export default class LoginPage extends React.Component {
   render() {
     return (
       <div className={styles.content}>
-        <h1 className={styles.heading}>Login Page</h1>
+        <h1 className={styles.heading}>Login Page 23</h1>
         <p className={styles.lead}>Create an account to get started!</p>
         <button className={styles.signUpButton} onClick={this.signUp}>Sign up</button>
       </div>
